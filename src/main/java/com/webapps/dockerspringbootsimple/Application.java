1a1e177b75763ba94d57a510c5775cb57a88620b
Kwensy <279529@supinfo.com>
2019-04-08 19:25:56 +0200

    Splitting controller and main application class

--- src/main/java/com/webapps/dockerspringbootsimple/Application.java
+++ src/main/java/com/webapps/dockerspringbootsimple/Application.java
@@ -8,22 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
-@RestController
 public class Application{
     
     public static void main(String [] args){
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Value("${spring.application.name}")
-    private String instanceName;
-
-    @GetMapping("/")
-    public String hello(){
-        if(instanceName == null || instanceName.length() == 0)
-                throw new RuntimeException("spring.application.name property is empty."+
-                 "Please rerun the application with the "+
-                 "option --spring.application.name=<applicationName>");
-        return "Response from " + instanceName + " at " + System.currentTimeMillis();
     }
 }
---
+++ src/main/java/com/webapps/dockerspringbootsimple/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.webapps.dockerspringbootsimple.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @Value("${spring.application.name}")
+    private String instanceName;
+
+    @GetMapping("/")
+    public String hello(){
+        if(instanceName == null || instanceName.length() == 0)
+                throw new RuntimeException("spring.application.name property is empty."+
+                 "Please rerun the application with the "+
+                 "option --spring.application.name=<applicationName>");
+        return "Response from " + instanceName + " at " + System.currentTimeMillis();
+    }
+}
